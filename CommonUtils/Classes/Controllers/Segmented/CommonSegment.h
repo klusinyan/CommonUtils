c4f8994a9af2865e0e12bc8489879ec16d21e0a4
Karen Lusinyan <karen.lusinyan.developerios@gamil.com>
2014-10-12 15:26:44 +0200

    Common progress (bundle not git ignote)

---
+++ CommonUtils/Classes/Controllers/Segmented/CommonSegment.h
@@ -0,0 +1,13 @@
+//
+//  CommonSegmented.h
+//  CommonUtils
+//
+//  Created by Karen Lusinyan on 10/10/14.
+//  Copyright (c) 2014 Karen Lusinyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommonSegmented : UIViewController
+
+@end
---
+++ CommonUtils/Classes/Controllers/Segmented/CommonSegment.m
@@ -0,0 +1,49 @@
+//
+//  CommonSegmented.m
+//  CommonUtils
+//
+//  Created by Karen Lusinyan on 10/10/14.
+//  Copyright (c) 2014 Karen Lusinyan. All rights reserved.
+//
+
+#import "CommonSegmented.h"
+
+@interface CommonSegmented ()
+
+@end
+
+@implementation CommonSegmented
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
