5405f7721d7665167ae087842d728d065cc201ae
Karen Lusinyan <karen.lusinyan.developerios@gamil.com>
2014-10-13 10:05:58 +0200

    common progress

--- CommonUtilsTest/Classes/CommonProgress/CommonProgressViewController.m
+++ CommonUtilsTest/Classes/CommonProgress/CommonProgressViewController.m
@@ -37,6 +37,7 @@
     [CommonProgress showWithTaregt:self completion:^{
         DebugLog(@"common progress did start");
     }];
+    
     [CommonProgress sharedProgress].activityIndicatorViewStyle = (random) ? CommonProgressActivityIndicatorViewStyleLarge : CommonProgressActivityIndicatorViewStyleNormal;
     
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
