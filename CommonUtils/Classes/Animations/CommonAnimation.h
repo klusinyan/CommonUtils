3f49ae2f8d89073ce08e31f475b3f7944ca86b63
Karen Lusinyan <karen.lusinyan@softecspa.it>
2015-10-27 16:54:54 +0100

    Ok

--- CommonUtils/Classes/Animations/CommonAnimation.h
+++ CommonUtils/Classes/Animations/CommonAnimation.h
@@ -1,8 +1,8 @@
 //  Created by Karen Lusinyan on 02/03/15.
 //  Copyright (c) 2015 Karen Lusinyan. All rights reserved.
 
-#import <CSAnimation.h>
-#import <CSAnimationView.h>
+#import "CSAnimation.h"
+#import "CSAnimationView.h"
 
 @interface CommonAnimation : NSObject
 
